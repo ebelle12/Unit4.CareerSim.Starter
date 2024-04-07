9faa67d27d33d2d4f2fa70be8bbd21e35f39bca5
Eva B <84365679+ebelle12@users.noreply.github.com>
2024-04-07 12:11:17 -0500

    update index.js
    changed name to product

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
-// fetchUserInfo
+// Finished
 // login
 // logout
-// createProduct // admin only
-// updateProduct // admin only
-// deleteProduct // admin only
-// addToCart 
-// removeFromCart
-// checkout ---- which will delete
+// *createProduct // admin only
+// *addToCart*
+// *removeFromCart*
+// list a single product*
 
-// Missing list a single product
+// Todo: 
+// *fetchUserInfo
+
+// *updateProduct // admin only
+// *deleteProduct // admin only
+// *checkout ---- which will delete*
+
 
 const pg = require('pg')
 
@@ -22,6 +26,7 @@ const {
     createProduct,
     fetchUsers,
     fetchProducts,
+    fetchProductById,
     fetchCarts,
     fetchCartProducts,
     deleteCarts,
@@ -29,6 +34,8 @@ const {
     findUserWithToken,
     createCart,
     updateCart,
+    isLoggedIn,
+    createSingleProduct
 
 
 } = require('./db');
@@ -64,9 +71,21 @@ app.get('/api/products', async (req, res, next) => {
     }
 });
 
+
+
 app.get('/api/products/:productId', async (req, res, next) => {
     try {
+        res.send(await fetchProductById(req.params.productId));
 
+    }
+    catch (ex) {
+        next(ex);
+    }
+});
+
+app.post("/api/products", async (req, res, next) => {
+    try {
+        res.send(await createProduct(req.body));
     }
     catch (ex) {
         next(ex);
@@ -138,6 +157,21 @@ app.post('/api/user/auth', async (req, res, next) => {
         next(ex);
     }
 });
+
+app.post('/api/products', isLoggedIn, async (req, res, next) => {
+    try {
+        console.log(req.user)
+        if (req.user.admin) {
+            const result = await createProduct(req.body.name, req.body.description, req.body.photos, req.body.price, req.body.inventory)
+            res.status(201).send(result)
+        }
+        else { res.send({ status: 401, message: "User Not Admin" }) };
+    }
+
+    catch (ex) {
+        next(ex);
+    }
+});
 app.post('/api/users/cart_products', async (req, res, next) => {
     try {
         const userId = await findUserWithToken(req.body.token)
@@ -145,7 +179,7 @@ app.post('/api/users/cart_products', async (req, res, next) => {
             res.send({ status: 400, message: "User Not Logged" });
             return;
         }
-        res.status(201).send(await createCart({ user_id: userId.id, product_id: req.body.product_id, amount: req.body.amount }));
+        res.status(201).send(await addToCart({ user_id: userId.id, product_id: req.body.product_id, amount: req.body.amount }));
     }
     catch (ex) {
         next(ex);
@@ -165,14 +199,14 @@ app.put('/api/users/cart_products', async (req, res, next) => {
     }
 });
 
-app.delete('/api/users/cart/:product_id', isLoggedIN, async (req, res, next) => {
+app.delete('/api/users/cart/:product_id', isLoggedIn, async (req, res, next) => {
     try {
         const userId = await findUserWithToken(req.body.token)
         if (!userId) {
             res.send({ status: 400, message: "User Not Logged" });
             return;
         }
-        await deleteCarts({ user_id: userId.id, product_id: req.params.product_id });
+        await removeFromCart({ user_id: userId.id, product_id: req.params.product_id });
         res.send({ status: 200, message: "cart deleted", userId: userId.id, productId: req.params.product_id })
 
     }
