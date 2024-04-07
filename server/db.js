c37efb2769bce8f6fcedd429deec3f492a55a302
Eva B <84365679+ebelle12@users.noreply.github.com>
2024-04-07 12:08:08 -0500

    update db.js
    update db.js

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const JWT = process.env.JWT || 'shhh';
 
 const createTables = async () => {
-    const SQL = `
+  const SQL = `
 
 DROP TABLE IF EXISTS cart_products;
 DROP TABLE IF EXISTS products;
@@ -39,189 +39,216 @@ CREATE TABLE users(
 
 
 `
-    await client.query(SQL);
+  await client.query(SQL);
 };
 
-const createUser = async ({ username, password, name, email }) => {
-    const SQL = `
-    INSERT INTO users(username, password,name,email) VALUES($1, $2, $3, $4) RETURNING *
+const createUser = async ({ username, password, name, email, admin }) => {
+  const SQL = `
+    INSERT INTO users(username, password,name,email,admin) VALUES($1, $2, $3, $4, $5) RETURNING *
   `;
-    const response = await client.query(SQL, [username, await bcrypt.hash(password, 5), name, email]);
-    console.log(response);
-    return response.rows[0];
+  const response = await client.query(SQL, [username, await bcrypt.hash(password, 5), name, email, admin]);
+  console.log(response);
+  return response.rows[0];
 };
 
-const createProduct = async ({ name, description, photos, price, inventory }) => {
-    const SQL = `
+const createProduct = async (name, description, photos, price, inventory) => {
+  const SQL = `
     INSERT INTO products(name,description, photos, price,inventory) VALUES($1,$2,$3, $4,$5) RETURNING *
   `;
-    const response = await client.query(SQL, [name, description, photos, price, inventory]);
-    console.log(response);
-    return response.rows[0];
+  const response = await client.query(SQL, [name, description, photos, price, inventory]);
+  console.log(response);
+  return response.rows[0];
 };
 
-const createCart = async ({ user_id, product_id, amount }) => {
-    const SQL = `
+const creatSingleProduct = async (name, description, photos, price,) => {
+  const SQL = `
+    INSERT INTO products(name,description, photos, price,) VALUES($1,$2,$3, $4) RETURNING *
+  `;
+  const response = await client.query(SQL, [name, description, photos, price,]);
+  console.log(response);
+  return response.rows[0];
+};
+
+
+const addToCart = async ({ user_id, product_id, amount }) => {
+  const SQL = `
     INSERT INTO cart_products(id, user_id, product_id, amount) VALUES($1, $2, $3, $4) RETURNING *
   `;
-    const response = await client.query(SQL, [uuid.v4(), user_id, product_id, amount]);
-    return response.rows[0];
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id, amount]);
+  return response.rows[0];
 };
 const updateCart = async ({ user_id, product_id, amount }) => {
-    const SQL = `
+  const SQL = `
     UPDATE cart_products SET amount=$1 WHERE product_id=$2 AND user_id=$3
   `;
-    const response = await client.query(SQL, [amount, product_id, user_id]);
-    return { status: 200, message: "cart updated" };
+  const response = await client.query(SQL, [amount, product_id, user_id]);
+  return { status: 200, message: "cart updated" };
 };
 const createCartProducts = async ({ user_id, product_id }) => {
-    const SQL = `
+  const SQL = `
       INSERT INTO cart(id, user_id, product_id) VALUES($1, $2, $3) RETURNING *
     `;
-    const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
 
-    return response.rows[0];
+  return response.rows[0];
 };
 
-const deleteCarts = async ({ user_id, product_id }) => {
-    console.log("deleteCarts:", user_id, product_id)
-    const SQL = `
+const removeFromCart = async ({ user_id, product_id }) => {
+  console.log("deleteCarts:", user_id, product_id)
+  const SQL = `
     DELETE FROM cart_products WHERE user_id=$1 AND product_id=$2
   `;
-    await client.query(SQL, [user_id, product_id]);
+  await client.query(SQL, [user_id, product_id]);
 };
 
 const authenticate = async ({ username, password }) => {
-    const SQL = `
+  const SQL = `
     SELECT id, password, username 
     FROM users 
     WHERE username=$1;
   `;
-    console.log("AUTH:", username)
-    const response = await client.query(SQL, [username]);
-    console.log("RESP:", response);
-    if ((!response.rows.length || await bcrypt.compare(password, response.rows[0].password)) === false) {
-        const error = Error('not authorized');
-        error.status = 401;
-        throw error;
-    }
+  console.log("AUTH:", username)
+  const response = await client.query(SQL, [username]);
+  console.log("RESP:", response);
+  if ((!response.rows.length || await bcrypt.compare(password, response.rows[0].password)) === false) {
+    const error = Error('not authorized');
+    error.status = 401;
+    throw error;
+  }
 
-    const token = await jwt.sign({ id: response.rows[0].id }, JWT);
-    return { token: token };
+  const token = await jwt.sign({ id: response.rows[0].id }, JWT);
+  return { token: token };
 };
 
 const isLoggedIn = async (req, res, next) => {
-    try {
-        req.user = await findUserWithToken(req.headers.authorization);
-        next();
-    }
-    catch (ex) {
-        next(ex);
-    }
+  try {
+    console.log(req.headers.authorization, "WITHIN ISLOGGEDIN")
+    req.user = await findUserWithToken(req.headers.authorization);
+
+    next();
+  }
+  catch (ex) {
+    next(ex);
+  }
 };
 
-const createProductAdmin = async ({ user_id, product_id, amount }) => {
-    const SQL = `
-    INSERT INTO products(id, user_id, product_id, amount) VALUES($1, $2, $3, $4) RETURNING *
+const createProductAdmin = async ({ name, description, photos, price, inventory }) => {
+  const SQL = `
+    INSERT INTO products(name, description, photos, price, inventory) VALUES($1, $2, $3, $4) RETURNING *
   `;
-    const response = await client.query(SQL, [uuid.v4(), user_id, product_id, amount]);
-    return response.rows[0];
+  const response = await client.query(SQL, [name, description, photos, price, inventory]);
+  return response.rows[0];
 };
-const updateProductAdmin = async ({ id, amount }) => {
-    const SQL = `
+const updateProductAdmin = async ({ id, name, description, photos, price, inventory }) => {
+  const SQL = `
     UPDATE products SET amount=$1 WHERE id=$2
   `;
-    const response = await client.query(SQL, [amount, id,]);
-    return { status: 200, message: "cart updated" };
+  const response = await client.query(SQL, [id, name, description, photos, price, inventory]);
+  return { status: 200, message: "cart updated" };
 };
 
-const deleteProductAdmin = async ({ user_id, product_id }) => {
-    console.log("deleteProductAdmin:", user_id, product_id)
-    const SQL = `
-    DELETE FROM products WHERE user_id=$1 AND product_id=$2
+const deleteProductAdmin = async ({ id }) => {
+  console.log("deleteProductAdmin:", id)
+  const SQL = `
+    DELETE FROM products WHERE id =$1
   `;
-    await client.query(SQL, [user_id, product_id]);
+  await client.query(SQL, [id]);
 };
 /*
 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNzExNDk3ODY3fQ.nl9-6mrjlc3M-GCKo-q0wmX7BKbhBeaN1orr2V-bEP0
 */
 const findUserWithToken = async (token) => {
-    let id;
-    console.log("insidefinduserwithtoken")
-    console.log("passed token " + token)
-    try {
-        const payload = await jwt.verify(token, JWT);
-        id = payload.id;
-        console.log("ID:", id);
-        return { id: id };
-    } catch (ex) {
-        const error = Error('not authorized');
-        error.status = 401;
-        throw error;
+  let id;
+  console.log("insidefinduserwithtoken")
+  console.log("passed token " + token)
+  try {
+    const payload = await jwt.verify(token, JWT);
+    id = payload.id;
+    const sql = `SELECT admin from users WHERE id = $1`
+    const res = await client.query(sql, [payload.id])
+    const { admin } = res.rows[0]
+    console.log("admin", res.rows[0])
+    console.log("ID:", id);
+    return { id: id, admin: admin };
+  } catch (ex) {
+    const error = Error('not authorized');
+    error.status = 401;
+    throw error;
 
-    }
-    const SQL = `
+  }
+  const SQL = `
     SELECT id, username FROM users WHERE id=$1;
   `;
-    const response = await client.query(SQL, [id]);
-    if (!response.rows.length) {
-        const error = Error('not authorized');
-        error.status = 401;
-        throw error;
-    }
-    return response.rows[0];
+  const response = await client.query(SQL, [id]);
+  if (!response.rows.length) {
+    const error = Error('not authorized');
+    error.status = 401;
+    throw error;
+  }
+  return response.rows[0];
 };
 
 const fetchUsers = async () => {
-    const SQL = `
+  const SQL = `
     SELECT id, username,name, email FROM users;
   `;
-    const response = await client.query(SQL);
-    return response.rows;
+  const response = await client.query(SQL);
+  return response.rows;
 };
 
 const fetchProducts = async () => {
-    console.log("fetchProducts");
-    const SQL = "SELECT * FROM products";
+  console.log("fetchProducts");
+  const SQL = "SELECT * FROM products";
 
 
-    const response = await client.query(SQL);
-    console.log("RESP", response);
-    return response.rows;
+  const response = await client.query(SQL);
+  console.log("RESP", response);
+  return response.rows;
 };
 const fetchCarts = async () => {
-    const SQL = `
+  const SQL = `
       SELECT * FROM carts;
     `;
-    const response = await client.query(SQL);
-    return response.rows;
+  const response = await client.query(SQL);
+  return response.rows;
 };
 
-const fetchCartProducts = async (user_id) => {
-    const SQL = `
-    SELECT * FROM cart_products where user_id = $1
+const fetchCartProducts = async (id) => {
+  const SQL = `
+    SELECT * FROM cart_products where id = $1
   `;
-    const response = await client.query(SQL, [user_id]);
-    return response.rows;
+  const response = await client.query(SQL, [id]);
+  return response.rows;
+};
+
+const fetchProductById = async (product_id) => {
+  const SQL = `
+    SELECT * FROM products where id = $1
+  `;
+  const response = await client.query(SQL, [product_id]);
+  return response.rows[0];
 };
 
 module.exports = {
-    client,
-    createTables,
-    createUser,
-    createProduct,
-    createCart,
-    createCartProducts,
-    createProductAdmin,
-    updateProductAdmin,
-    deleteProductAdmin,
-    fetchUsers,
-    fetchProducts,
-    fetchCarts,
-    fetchCartProducts,
-    deleteCarts,
-    authenticate,
-    findUserWithToken,
-    updateCart,
-    isLoggedIn,
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  createCart,
+  createCartProducts,
+  createProductAdmin,
+  updateProductAdmin,
+  deleteProductAdmin,
+  fetchUsers,
+  fetchProducts,
+  fetchProductById,
+  fetchCarts,
+  fetchCartProducts,
+  deleteCarts,
+  authenticate,
+  findUserWithToken,
+  updateCart,
+  isLoggedIn,
+  createSingleProduct
+
 };
